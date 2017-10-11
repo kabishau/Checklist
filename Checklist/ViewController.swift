bcf5cada84f76a39cadb4a69bd4d7a74da8bfb6e
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2017-10-10 20:37:45 -0400

    Initial Commit

---
+++ Checklist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checklist
+//
+//  Created by Aleksey Kabishau on 1010..17.
+//  Copyright © 2017 Dreamline. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
