c89d36f67d1af69b13d68c6eb584e9819dd01f9c
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2017-10-10 23:08:05 -0400

    reusable cell added access through the code to label of prototype cell

--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -12,14 +12,28 @@ class ChecklistViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        // creating label with specific tag
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row == 0 {
+            label.text = "Run a marathon"
+        } else {
+            label.text = "Sleep"
+        }
+        
+        
+        return cell
+        
+    }
 
 }
-
