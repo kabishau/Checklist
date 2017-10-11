1b15e0b6da8977734403beff517c32ebcfff405e
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2017-10-11 01:30:03 -0400

    selecting cell delegate has been implemented row is unhighlighted right after it was selected

--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -15,8 +15,24 @@ class ChecklistViewController: UITableViewController {
 
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        //
+        if let cell = tableView.cellForRow(at: indexPath) {
+            // selecting and unselecting the cell depending on current state
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+        }
+
+        // removing highlighting the selected cell
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 100
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -25,13 +41,23 @@ class ChecklistViewController: UITableViewController {
         // creating label with specific tag
         let label = cell.viewWithTag(1000) as! UILabel
         
-        if indexPath.row == 0 {
-            label.text = "Run a marathon"
-        } else {
+        
+        switch indexPath.row % 5 {
+        case 0:
+            label.text = "Walk the dog"
+        case 1:
+            label.text = "Brush my teeth"
+        case 2:
+            label.text = "Learn iOS Development"
+        case 3:
+            label.text = "Soccer practice"
+        case 4:
+            label.text = "Eat ice cream"
+        default:
             label.text = "Sleep"
         }
         
-        
+
         return cell
         
     }
